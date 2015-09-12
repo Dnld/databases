3a9b62ecd13096ea2dce80e9b2ff1fcb437ea29d
Dnld <steinert.donald@gmail.com>
2015-09-11 23:20:23 -0400

    implements db, get and request handling

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,12 +1,37 @@
 var models = require('../models');
 var bluebird = require('bluebird');
 
-
+var statusCode = 200;
+var headers = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10 // Seconds.
+};
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    get: function (req, res) {
+      //headers['Allow'] = "GET, POST, PUT, DELETE, OPTIONS";
+      headers['Content-Type'] = "application/JSON";
+      res.writeHead(statusCode, headers);
+      res.end();
+      // console.log('get request received');
+      // console.log('get url: ' + req.url);
+    }, // a function which handles a get request for all messages
+    post: function (req, res) {
+      //headers['Allow'] = "GET, POST, PUT, DELETE, OPTIONS";
+      res.writeHead(statusCode, headers);
+      res.end();
+      console.log('post request received');
+      console.log('post url: ' + req.url);
+    }, // a function which handles posting a message to the database
+    options: function(req, res) {
+      console.log('options request received');
+      headers['Allow'] = "GET, POST, OPTIONS";
+      res.writeHead(statusCode, headers);
+      res.end();
+    }
   },
 
   users: {
--- server/schema.sql
+++ server/schema.sql
@@ -4,8 +4,24 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  messageid int(6),
+  date varchar(40),
+  text text(250),
+  userid int(7),
+  roomid int(3)
 );
 
+CREATE TABLE users (
+  /* Describe your table here.*/
+  userid int(7),
+  username varchar(20)
+);
+
+CREATE TABLE rooms (
+  /* Describe your table here.*/
+  roomid int(3),
+  roomname varchar(20)
+);
 /* Create other tables and define schemas for them here! */
 
 
